1f9520ec9c6c5c631873ae6d07a7bd67cc4c80a7
fuping <76337559@qq.com>
2022-10-24 09:56:00 +0800

    add 完成选举函数electionLoop

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -20,6 +20,7 @@ package raft
 import (
 	//	"bytes"
 
+	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -213,21 +214,48 @@ type AppendEntriesReply struct {
 //
 // example RequestVote RPC handler.
 //
-func (rf *Raft) RequestVote(request *RequestVoteArgs, response *RequestVoteReply) {
+func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
 	rf.mu.Lock()
 	defer rf.mu.Unlock()
-	defer rf.persist()
-	// defer DPrintf("{Node %v}'s state is {state %v,term %v,commitIndex %v,lastApplied %v} before processing requestVoteRequest %v and reply requestVoteResponse %v", rf.me, rf.state, rf.currentTerm, rf.commitIndex, rf.lastApplied, request, response)
 
-	if request.Term < rf.currentTerm || (request.Term == rf.currentTerm && rf.votedFor != -1 || rf.votedFor != request.CandidateId) {
-		response.Term, response.VoteGranted = rf.currentTerm, false
-	}
+	reply.Term = rf.currentTerm
+	reply.VoteGranted = false
 
-	if request.Term > rf.currentTerm {
-		//rf.ChangeState(StateFollower)
-		rf.currentTerm, rf.votedFor = request.Term, -1
+	DPrintf("RaftNode[%d] Handle RequestVote, CandidatesId[%d] Term[%d] CurrentTerm[%d] LastLogIndex[%d] LastLogTerm[%d] votedFor[%d]",
+		rf.me, args.CandidateId, args.Term, rf.currentTerm, args.LastLogIndex, args.LastLogTerm, rf.votedFor)
+	defer func() {
+		DPrintf("RaftNode[%d] Return RequestVote, CandidatesId[%d] VoteGranted[%v] ", rf.me, args.CandidateId, reply.VoteGranted)
+	}()
+
+	// 任期不如我大，拒绝投票
+	if args.Term < rf.currentTerm {
+		return
 	}
+	// 发现更大的任期，则转为该任期的follower
+	if args.Term > rf.currentTerm {
+		rf.currentTerm = args.Term
+		rf.role = ROLE_FOLLOWER
+		rf.votedFor = -1
+		rf.leaderId = -1
+	}
+	// 进行投票，每个任期，只能投票给一人
+	if rf.votedFor == -1 || rf.votedFor == args.CandidateId {
+		// candidate的日志必须比我的新
+		// 1. 最后一条log，任期大的更新
+		// 2. 更长的log则更新
+		lastLogTerm := 0
+		if len(rf.logs) != 0 {
+			lastLogTerm = rf.logs[len(rf.logs)-1].Term
+		}
+		if args.LastLogTerm < lastLogTerm || args.LastLogIndex < len(rf.logs) {
+			return
+		}
+		rf.votedFor = args.CandidateId
+		reply.VoteGranted = true
+		rf.lastActiveTime = time.Now() // 为其他人投票，那么重置自己的下次投票时间
+	}
+	rf.persist()
 
 }
 
@@ -360,6 +388,119 @@ func Make(peers []*labrpc.ClientEnd, me int,
 
 func (rf *Raft) electionLoop() {
 	// TODO
+	for !rf.killed() {
+		time.Sleep(1 * time.Millisecond)
+
+		func() {
+			rf.mu.Lock()
+			defer rf.mu.Unlock()
+
+			now := time.Now()
+			timeout := time.Duration(200+rand.Int31n(150)) * time.Millisecond // 超时随机化
+			elapses := now.Sub(rf.lastActiveTime)
+			// follower -> candidates
+			if rf.role == ROLE_FOLLOWER {
+				if elapses >= timeout {
+					rf.role = ROLE_CANDIDATES
+					DPrintf("RaftNode[%d] Follower -> Candidate", rf.me)
+				}
+			}
+			// 请求vote
+			if rf.role == ROLE_CANDIDATES && elapses >= timeout {
+				rf.lastActiveTime = now // 重置下次选举时间
+
+				rf.currentTerm += 1 // 发起新任期
+				rf.votedFor = rf.me // 该任期投了自己
+				rf.persist()
+
+				// 请求投票
+				args := RequestVoteArgs{
+					Term:         rf.currentTerm,
+					CandidateId:  rf.me,
+					LastLogIndex: len(rf.logs),
+				}
+				if len(rf.logs) != 0 {
+					args.LastLogTerm = rf.logs[len(rf.logs)-1].Term
+				}
+				rf.mu.Unlock()
+
+				DPrintf("RaftNode[%d] RequestVote starts, Term[%d] LastLogIndex[%d] LastLogTerm[%d]", rf.me, args.Term, args.LastLogIndex, args.LastLogTerm)
+
+				// 并发RPC请求vote
+				type VoteResult struct {
+					peerId int
+					resp   *RequestVoteReply
+				}
+				voteCount := 1   // 收到投票个数（先给自己投1票）
+				finishCount := 1 // 收到应答个数
+				voteResultChan := make(chan *VoteResult, len(rf.peers))
+				for peerId := 0; peerId < len(rf.peers); peerId++ {
+					go func(id int) {
+						if id == rf.me {
+							return
+						}
+						resp := RequestVoteReply{}
+						if ok := rf.sendRequestVote(id, &args, &resp); ok {
+							voteResultChan <- &VoteResult{
+								peerId: id,
+								resp:   &resp,
+							}
+						} else {
+							voteResultChan <- &VoteResult{
+								peerId: id,
+								resp:   nil,
+							}
+						}
+					}(peerId)
+				}
+
+				maxTerm := 0
+				for {
+					select {
+					case voteResult := <-voteResultChan:
+						finishCount += 1
+						if voteResult.resp != nil {
+							if voteResult.resp.VoteGranted {
+								voteCount += 1
+							}
+							if voteResult.resp.Term > maxTerm {
+								maxTerm = voteResult.resp.Term
+							}
+						}
+						// 得到大多数vote后，立即离开
+						if finishCount == len(rf.peers) || voteCount > len(rf.peers)/2 {
+							goto VOTE_END
+						}
+					}
+				}
+			VOTE_END:
+				rf.mu.Lock()
+				defer func() {
+					DPrintf("RaftNode[%d] RequestVote ends, finishCount[%d] voteCount[%d] Role[%s] maxTerm[%d] currentTerm[%d]", rf.me, finishCount, voteCount, rf.role, maxTerm, rf.currentTerm)
+				}()
+				// 如果角色改变了，则忽略本轮投票结果
+				if rf.role != ROLE_CANDIDATES {
+					return
+				}
+				// 发现更高的任期，切回follower
+				if maxTerm > rf.currentTerm {
+					rf.role = ROLE_FOLLOWER
+					rf.leaderId = -1
+					rf.currentTerm = maxTerm
+					rf.votedFor = -1
+					rf.persist()
+					return
+				}
+				// 赢得大多数选票，则成为leader
+				if voteCount > len(rf.peers)/2 {
+					rf.role = ROLE_LEADER
+					rf.leaderId = rf.me
+					rf.lastBroadcastTime = time.Unix(0, 0) // 立即执行appendEntries广播
+					return
+				}
+			}
+		}()
+	}
 }
 
 func (rf *Raft) appendEntriesLoop() {
